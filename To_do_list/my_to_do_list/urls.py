f6909bfd38eb8ae9f572af593de1060fe9a3224a
Dipesh <dipeshluitel40@gmail.com>
2025-11-04 10:46:03 +0545

    view for save html is created and linked into urls.py(app)

--- To_do_list/my_to_do_list/urls.py
+++ To_do_list/my_to_do_list/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from my_to_do_list import views
 
 urlpatterns = [
-    path('',views.active, name='active')
+    path('',views.active, name='active'),
+    path('/saved',views.save, name='save'),
 ]
--- To_do_list/my_to_do_list/views.py
+++ To_do_list/my_to_do_list/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def active(request):
     return render(request,'active.html')
+
+def save(request):
+    return render(request,'save.html')
