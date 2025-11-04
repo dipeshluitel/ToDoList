1355215b1bb459853175cff4473dcff393e0d3af
Dipesh <dipeshluitel40@gmail.com>
2025-11-04 23:04:37 +0545

    UserProfile model created for user authentication

--- To_do_list/my_to_do_list/models.py
+++ To_do_list/my_to_do_list/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class To_do_list(models.Model):
@@ -7,4 +8,6 @@ class To_do_list(models.Model):
 
     def __str__(self):
         return self.listed
-    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
